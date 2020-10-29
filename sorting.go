13346961a14f6811fb9942f575a4500f76fa4306
petikosa <petikosa@yahoo.com>
2020-10-29 08:14:16 +0100

    insert sort added

--- sorting.go
+++ sorting.go
@@ -6,7 +6,7 @@ const maxInt = int(^uint(0) >> 1)
 
 func main() {
 	a := []int{5, 8, 2, 5, 4, 9, 7, 8, 5, 3, 1, 6}
-	fmt.Println(selectSort(a))
+	fmt.Println(insertSort(a))
 }
 
 func findMin(a []int) (int, int) {
@@ -31,4 +31,30 @@ func selectSort(a []int) []int {
 		a = append(a[:minIndex], a[minIndex+1:]...)
 	}
 	return b
+}
+
+func insertSort(a []int) []int {
+	arrSize := len(a)
+	for i := 1; i < arrSize; i++ {
+		fmt.Println("before", a)
+		j := i - 1
+		update := false
+		for a[i] < a[j] {
+			update = true
+			j--
+			if j < 0 {
+				j = -1
+				break
+			}
+		}
+		if update {
+			// insert
+			a = append(a[:j+1], append([]int{a[i]}, a[j+1:]...)...)
+			// delete
+			a = append(a[:i+1], a[i+2:]...)
+		}
+
+		fmt.Println("after ", a)
+	}
+	return a
 }
